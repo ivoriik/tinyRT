004627786c90254afd0d86121ebe13302c021592
vbespalk <nama_om@ymail.com>
2019-02-11 14:45:15 +0200

    lib

--- libft/ft_swap.c
+++ libft/ft_swap.c
@@ -16,7 +16,7 @@ void	ft_swap(void *nb1, void *nb2, size_t size)
 {
 	unsigned char	swap_tmp[size];
 
-	ft_memcpy(&(*swap_tmp), &(*nb1), size);
-	ft_memcpy(&(*nb1), &(*nb2), size);
-	ft_memcpy(&(*nb2), &(*swap_tmp), size);
+	ft_memcpy(&(*swap_tmp), (nb1), size);
+	ft_memcpy((nb1), (nb2), size);
+	ft_memcpy((nb2), &(*swap_tmp), size);
 }
